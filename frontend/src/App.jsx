37bb27a36584e4cc72a8e7ee03f783b148bb045c
Rvaindra7330 <haravinadona.ravindra@gmail.com>
2024-01-27 19:43:41 +0530

    added signin and signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-
+import React from 'react';
+import { BrowserRouter,Routes,Route, useNavigate } from 'react-router-dom';
+import { Signup } from './components/signup';
+import { Signin } from './components/signin';
 function App() {
 
   return (
     <div>
-        Hello world
-    </div>
-  )
+      <BrowserRouter>
+      <div>
+        <button onClick={()=>{
+          window.location.href="/signup"
+        }}>sign Up page</button>
+        <button onClick={()=>{
+          window.location.href="/signin"
+        }}>sign in page</button>
+      </div>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>)
+  
 }
 
-export default App
+export default App;
